b3ac6d1604b45d3e43a518d590bb43247173d626
Rguptaah <sidarya321@gmail.com>
2022-06-21 11:27:10 +0530

    dropzone practice

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dropzone Upload</title>
+    <link href="assets/css/dropzone.min.css" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+    <form action="upload.php" class="dropzone"></form>
+    <script src="assets/js/dropzone.min.js"></script>
+</body>
+
+</html>
